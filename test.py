da682f89f9a75fd72ecddec5b9a6e260017b7529
Luis Gallet <nanogallet@gmail.com>
2016-04-03 15:55:18 -0400

    changed smth

--- test.py
+++ test.py
@@ -14,14 +14,14 @@ import math
 # main tester function
 def main():
     # basic encryption example
-    a = 15
-    b = 15
-    n = 143
+    #a = 9
+    #b = 7
+    #n = 143
 
     # basic decryption example
-    # a = 48
-    # b = 103
-    # n = 143
+    a = 48
+    b = 103
+    n = 143
 
     # new example
     # a = 2 # encrypt
@@ -40,10 +40,10 @@ def main():
     #n = 145906768007583323230186939349070635292401872375357164399581871019873438799005358938369571402670149802121818086292467422828157022922076746906543401224889672472407926969987100581290103199317858753663710862357656510507883714297115637342788911463535102712032765166518411726859837988672111837205085526346618740053
 
 
-    #print("result: " + str(mod_exp(a, b, n)))
-    print("compare: " + str((a*b) % n))
+    print("result: " + str(mod_exp(a, b, n)))
+    #print("compare: " + str((a*b) % n))
     #print("montgomery product: " + str(mont_result(a, b, n)))
-    print("montgomery product: " + str(mod_mult(a, b, n)))
+    #print("montgomery product: " + str(mod_mult(a, b, n)))
 	
 def mod_mult(a,b,N):
 
